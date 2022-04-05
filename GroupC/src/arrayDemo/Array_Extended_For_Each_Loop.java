1ce80757210e7582d3487e179f4deb935ec2934a
ledeta04 <ashenafichan@gmail.com>
2022-04-05 11:11:04 -0400

    commit new java

--- GroupC/src/arrayDemo/Array_Extended_For_Each_Loop.java
+++ GroupC/src/arrayDemo/Array_Extended_For_Each_Loop.java
@@ -5,7 +5,7 @@ public class Array_Extended_For_Each_Loop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int [] studentGrade = {55,65,72,75,85,95};
+		int [] studentGrade = {55,65,72,75,85,95,100};
 		
 		for(int grade: studentGrade) {
 			
